823ae0d9a9826bf698b2120175ad6d33c2a2bf74
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-24 18:14:10 +0900

    Blockchain 구현 #2  - install crypto-js  - 클래스에 static 메서드 추가: calculateBlockHash

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as cryptoJS from 'crypto-js';
+
+
 // BlockChain 만들기
 
 class Block {
@@ -6,6 +9,11 @@ class Block {
 	public previousHash: string;
 	public data: string;
 	public timestamp: number;
+
+	static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => {
+		return cryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+
+	}
 
 	constructor(
 		index: number,
@@ -16,21 +24,23 @@ class Block {
 	) {
 		this.index = index;
 		this.hash = hash;
-		this.previousHash =previousHash;
+		this.previousHash = previousHash;
 		this.data = data;
 		this.timestamp = timestamp;
 	}
 }
 
+// console.log('calculateBlockHash: ' + Block.calculateBlockHash(1, 'previous', 1234, 'data'))
 
-const genesisBlock: Block = new Block(0, 'hashnum1234', '', 'hello world', new Date().getTime() /1000);
-const secondBlock: Block = new Block(1, 'hashnum5678', genesisBlock.hash,'this is data!', new Date().getTime() / 1000)
+const genesisBlock: Block = new Block(0, Block.calculateBlockHash(1, '', 12345, 'hello world'), '', 'hello world', Math.round(new Date().getTime() / 1000));
+const secondBlock: Block = new Block(1,  Block.calculateBlockHash(2, 'haha', 111111, 'this is data'), genesisBlock.hash, 'this is data!', Math.round(new Date().getTime() / 1000))
 const blockchain: Block[] = [genesisBlock];
 
 blockchain.push(secondBlock)
 
-// block type이 아니면 블록체인에 등록 불가능!
-// blockchain.push('string block')
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length -1]
+const getTimestamp = (): number => Math.round(new Date().getTime() / 1000)
 
 console.log(blockchain)
 
