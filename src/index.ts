55422e7c85b05c898adb55c7c9ce308da41e64fd
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-24 17:01:03 +0900

    타입스크립트 인터페이스 선언 하여 객체 다루기  - 함수에 객체 전달할때 타입 지정

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,19 @@
-// console.log('hello world')
-
-
-// 1. 타입스크립트의 장점
-// 자바스크립트 느슨함을 없애고 엄격하게 시그니처를 유지해줘 버그를 줄여준다.
-
-const name = 'joy', age = 20, gender = 'male';
-
-// 성별 매개변수는 옵션으로 설정
-const printPerson = (name, age, gender?) => {
-	console.log(`hello. ${name} age: ${age}. you are a ${gender}`)
+interface Human {
+	name: string,
+	age: number,
+	married: boolean,
 }
 
-// 매개변수 - gender 누락시 컴파일 실패.
-// printPerson(name, age,)
-
-printPerson(name, age)
-
-
-
-
-// 2. 타입을 지정할 수 있어 망나니 js 를 java 처럼 엄격하게 사용 하여
-// 버그를 줄이고 결과 예측이 예상된다.
-const sayHi = (username: string, age: number, married: boolean): void => {
-	console.log(`username: ${username}, age: ${age}, married: ${married}`)
+const man = {
+	name: 'jojo',
+	age: 20,
+	married: false,
 }
 
-sayHi('jihoon', 33, true)
+const myNameIs = (person: Human): string => {
+	const { name, age, married } = person;
+	return `my name is ${name}. and I'm ${age} and ${married ? 'married' : 'single'}.`
+}
 
-
-// 3. 함수 리턴 타입 추가
-const sum = (a:number, b:number,):number => a + b
-console.log(sum(10, 20))
-
-
-export {}
+const str = myNameIs(man)
+console.log({ str })
