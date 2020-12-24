f19c185603a8aaafbba5fc8cbdf75b3ef3413c49
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-24 17:25:12 +0900

    타입스크립트 인터페이스 대신 클래스 선언 사용하기

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-interface Human {
-	name: string,
-	age: number,
-	married: boolean,
+
+// interface 는 타입스크립트 전용으로 컴파일되어도 자바스크립에 없음
+// interface Human {
+// 	name: string,
+// 	age: number,
+// 	married: boolean,
+// }
+
+// 자바스크립트에 모델이 코드할 경우 => class
+
+class Human {
+	public name: string;
+	public age: number;
+
+	// married 는 private 이므로 외부에서 접근 불가
+	// private married: boolean;
+	public married: boolean;
+
+	constructor(name: string, age: number, married: boolean) {
+		this.name = name;
+		this.age = age;
+		this.married = married;
+	}
 }
 
 const man = {
