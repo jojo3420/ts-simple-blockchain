8248e27093d54134435e34435e86c6d2ddc362ee
jojo3420 <jjjhhhvvv@gmail.com>
2020-12-24 16:03:44 +0900

    자바스크립트와 타입스크립트의 차이점 및 ts 장점  - ts: 느슨한 제거하여 예측 가능성 높힌다  - 개발자의 실수, 버그 최소화 할 수 있게 엄격하게 설계 되었다.

--- TYPE_CHAIN/index.ts
+++ TYPE_CHAIN/index.ts
@@ -1,1 +1,33 @@
-console.log('hello world')
+// console.log('hello world')
+
+
+// 1. 타입스크립트의 장점
+// 자바스크립트 느슨함을 없애고 엄격하게 시그니처를 유지해줘 버그를 줄여준다.
+
+const name = 'joy', age = 20, gender = 'male';
+
+// 성별 매개변수는 옵션으로 설정
+const printPerson = (name, age, gender?) => {
+	console.log(`hello. ${name} age: ${age}. you are a ${gender}`)
+}
+
+// 매개변수 - gender 누락시 컴파일 실패.
+// printPerson(name, age,)
+
+printPerson(name, age)
+
+
+
+
+// 2. 타입을 지정할 수 있어 망나니 js 를 java 처럼 엄격하게 사용 하여
+// 버그를 줄이고 결과 예측이 예상된다.
+const sayHi = (username: string, age: number, married: boolean) => {
+	console.log(`username: ${username}, age: ${age}, married: ${married}`)
+}
+
+sayHi('jihoon', 33, true)
+
+
+
+
+export {}
